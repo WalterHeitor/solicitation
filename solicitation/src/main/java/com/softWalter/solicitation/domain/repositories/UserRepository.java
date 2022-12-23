5d30a3652f6753731d3cb3cdde5e79cae0cfe7c7
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2022-12-23 00:18:23 -0300

    create user repository query

--- solicitation/src/main/java/com/softWalter/solicitation/domain/repositories/UserRepository.java
+++ solicitation/src/main/java/com/softWalter/solicitation/domain/repositories/UserRepository.java
@@ -2,8 +2,14 @@ package com.softWalter.solicitation.domain.repositories;
 
 import com.softWalter.solicitation.domain.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT * FROM User WHERE email = ?1 AND password = ?2")
+    public Optional<User> login(String email, String password);
 }
