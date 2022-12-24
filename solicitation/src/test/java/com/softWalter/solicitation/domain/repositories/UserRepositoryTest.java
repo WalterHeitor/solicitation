39b6c88410489964ff3d9583bfe9c2ea31906880
WalterHeitor <44624641+WalterHeitor@users.noreply.github.com>
2022-12-24 10:51:04 -0300

    implements test user repository

---
+++ solicitation/src/test/java/com/softWalter/solicitation/domain/repositories/UserRepositoryTest.java
@@ -0,0 +1,80 @@
+package com.softWalter.solicitation.domain.repositories;
+
+import com.softWalter.solicitation.domain.entities.User;
+import com.softWalter.solicitation.domain.enums.Role;
+import com.softWalter.solicitation.template.user.UserMockFacktory;
+import org.junit.FixMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    private User user = UserMockFacktory.createNewUser();
+    private  final String testFake = "Walter Heitor Test User Update";
+
+    @Test
+    @Order(0)
+    public void save_user_test(){
+
+        User userTest = userRepository.save(user);
+        assertThat(userTest.getId()).isEqualTo(1L);
+    }
+
+    @Test
+    @Order(1)
+    public void update_user_test(){
+
+        System.out.println(user.getId() + "Aki esta Id user");
+        user.setId(1L);
+        user.setName(testFake);
+        User userTest = userRepository.save(user);
+        assertThat(userTest.getId()).isEqualTo(1L);
+        assertThat(userTest.getName()).isEqualTo(testFake);
+    }
+
+    @Test
+    @Order(2)
+    void get_by_id_test() {
+
+        Optional<User> userTest = userRepository.findById(1L);
+        assertThat(userTest.get().getId()).isEqualTo(1L);
+        assertThat(userTest.get().getName()).isEqualTo(testFake);
+    }
+
+    @Test
+    @Order(3)
+    void list_users_test() {
+        List<User> usersTest = userRepository.findAll();
+        assertThat(usersTest.size()).isEqualTo(1);
+    }
+
+    @Test
+    @Order(4)
+    void get_login_test() {
+        String email = "walter@gmail.com";
+        String password = "123";
+        Optional<User> userTest = userRepository.login(email, password);
+        assertThat(userTest.get().getId()).isEqualTo(1L);
+        assertThat(userTest.get().getName()).isEqualTo(testFake);
+        assertThat(userTest.get().getEmail()).isEqualTo(email);
+        assertThat(userTest.get().getPassword()).isEqualTo(password);
+    }
+}
---
+++ solicitation/src/test/java/com/softWalter/solicitation/template/user/UserMockFacktory.java
@@ -0,0 +1,20 @@
+package com.softWalter.solicitation.template.user;
+
+import com.softWalter.solicitation.domain.entities.User;
+import com.softWalter.solicitation.domain.enums.Role;
+
+public class UserMockFacktory {
+
+
+    public static User createNewUser() {
+        return new User(
+                null,
+                "walter",
+                "walter@gmail.com",
+                "123",
+                Role.ADMINISTRATOR,
+                null,
+                null
+        );
+    }
+}
